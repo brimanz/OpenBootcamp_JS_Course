008a8c052a07619e3db3f2eec033c03f49f2a270
jonathan briceño <brimanz2014@gmail.com>
2022-09-22 08:41:51 -0400

    practica 2 JS

---
+++ ejercicio_2/ejercicio_2.js
@@ -0,0 +1,14 @@
+const persona = {
+	nombre: "Jonathan",
+	edad: 33,
+	eresDesarrollador: true,
+	fechaNacimiento: new Date(1989, 6, 27),
+	libroFavorito: {
+		titulo: "Conde de montecristo",
+		autor: "Alejandro Dumas",
+		fecha: 1844,
+		url: "https://wmagazin.com/relatos/el-conde-de-montecristo-de-dumas-el-clasico-que-regalaria-la-libreria-la-tertulia-de-la-habana/#el-libro-que-regalar%c3%adan-los-libreros-el-conde-de-montecristo-de-alejandro-dumas"
+	}
+}
+
+console.log(persona.eresDesarrollador);
